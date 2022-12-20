3e33942a037744e25739fb92dd33ee466428bb11
aSrijan <adhyayansrijan1998@gmail.com>
2022-12-20 13:16:21 +0530

    first commit of basepkg

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="basepkg",
+version="0.1",
+description="this is a basic calculator",
+long_description="use this to calculate",
+author="srijan",
+packages=["basepkg"],
+install_requires=[])
